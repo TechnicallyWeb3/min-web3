e9d27f9b854898f63343b451c1a9d7e2ec822ded
Chris <chriswindow466@gmail.com>
2025-06-19 23:06:01 +0530

    tab url click url removed

--- js/navbar/tabBar.js
+++ js/navbar/tabBar.js
@@ -97,11 +97,8 @@ const tabBar = {
 
     // click to enter edit mode or switch to a tab
     tabEl.addEventListener('click', function (e) {
-      if (tabs.getSelected() !== data.id) { // else switch to tab if it isn't focused
-        tabBar.events.emit('tab-selected', data.id)
-      } else { // the tab is focused, edit tab instead
-        tabEditor.show(data.id)
-      }
+      // Always just switch to the tab, do not show the tab editor
+      tabBar.events.emit('tab-selected', data.id)
     })
 
     tabEl.addEventListener('auxclick', function (e) {
